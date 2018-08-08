967e554ea90a4a4467349d26b3e72e194d9e8b94
Shawn Snyder <shawnsnyder@Shawns-MBP.grandenetworks.net>
2018-08-08 12:33:41 -0500

    static json

--- start.js
+++ start.js
@@ -1,5 +1,14 @@
-
 var express             = require( 'express' ),
     bodyParser          = require( 'body-parser' ),
     app                 = express();
+
+
+var path = require('path');
+
+
+
 app.use( bodyParser.json() );
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const server = app.listen(3005);
